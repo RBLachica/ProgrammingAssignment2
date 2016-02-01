d5879d84b81f1b93e51e9ffa2b4dba01669e19e0
RBLachica <ronaldlachica@gmail.com>
2016-02-01 15:24:27 -0600

    Update cachematrix.R
    Function that stores a matrix and cache's its inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##Function that stores a matrix and cache's its inverse.
+##1)set the value of matrix (mset)
+##2)get the value of matrix (mget)
+##3)set the value of inverse (iset)
+##4)get the value of inverse (iget)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL 						##Assign 'NULL' to inverse
+	mset <- function(y) {			
+		x <<- y 						##Set matrix 'x'
+		inverse <<- NULL
+	}
+	mget <- function() x 					        ##Return matrix 'x'
+	iset <- function(solve) inverse <<- solve 	                ##Cache the value of the inverse 
+	iget <- function() inverse 			        	##Return inverse
+	list(mset= mset, mget = mget,
+	     iset = iset,
+	     iget = iget)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##Function that calculates the inverse of matrix from the above function
+cacheSolve <- function(x, ...) {			                ##Return matrix that is the inverse of 'x'
+	inverse <- x$iget()				                ##Get inverse
+	if(!is.null(inverse)) {				                ##Check for the presence of inverse
+		message("getting cached data")	#Display message
+		return(inverse)
+	}
+	data <- x$mget()					        ##Get the atrix
+	inverse <- solve(data, ...)			                ##Use solve() to compute inverse
+	x$iset(inverse)					                ##Cache the inverse
+	inverse 						        ##Return the inverse
 }
+
